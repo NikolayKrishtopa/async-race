be81a366a387f5c14603f432336fb922a4d5df3a
Nikolay Krishtopa <nikolay.krishtopa@gmail.com>
2023-07-16 20:50:27 +0400

    feat: dynamic render header and switch mode

--- async-race/src/components/App.ts
+++ async-race/src/components/App.ts
@@ -1,17 +1,64 @@
 class App {
   container: HTMLElement;
+  mode: 'garage' | 'winners';
+  garageBtn: HTMLButtonElement | null;
+  winnersBtn: HTMLButtonElement | null;
 
   constructor() {
     this.container = document.querySelector('.root') as HTMLElement;
+    this.mode = 'garage';
+    this.garageBtn = null;
+    this.winnersBtn = null;
     this.initiate();
   }
 
-  initiate = () => {
-    console.log('I work');
+  createHeaderLayout = () => {
+    const header = document.createElement('header');
+    header.classList.add('header');
+    header.innerHTML = `
+     <h1 class="header__title">ASYNC RACE</h1>
+      <div class="header__btn-wrapper">
+        <button class="header__link" id="garageBtn">Garage</button>
+        <button class="header__link" id="winnersBtn">Winners</button>
+      </div>
+    `;
+    this.container.prepend(header);
+    this.garageBtn = document.querySelector('#garageBtn') as HTMLButtonElement;
+    this.winnersBtn = document.querySelector(
+      '#winnersBtn'
+    ) as HTMLButtonElement;
+    this.renderMode();
+  };
 
-    // this.container.innerHTML = `
-    // <p>App is running</p>
-    // `;
+  renderMode = () => {
+    if (!this.garageBtn || !this.winnersBtn) return;
+    if (this.mode === 'garage') {
+      this.garageBtn.classList.add('header__link_state_active');
+      this.winnersBtn.classList.remove('header__link_state_active');
+    } else if (this.mode === 'winners') {
+      this.winnersBtn.classList.add('header__link_state_active');
+      this.garageBtn.classList.remove('header__link_state_active');
+    }
+  };
+
+  setMode = (mode: typeof this.mode) => {
+    this.mode = mode;
+    this.renderMode();
+  };
+
+  setListeners = () => {
+    if (!this.garageBtn || !this.winnersBtn) return;
+    this.garageBtn.addEventListener('click', () => this.setMode('garage'));
+    this.winnersBtn.addEventListener('click', () => this.setMode('winners'));
+  };
+
+  createLayout = () => {
+    this.createHeaderLayout();
+  };
+
+  initiate = () => {
+    this.createLayout();
+    this.setListeners();
   };
 }
 
